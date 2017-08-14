8226caa400b5c24ddd66684310b67d2843868071
yegresvonason <yegresvonason@gmail.com>
2017-08-15 00:39:02 +0400

    menu-mobile-worked

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
+    var ishidden=true;
     $('.nav-icon').click(function(){
         $(this).toggleClass('open');
+        if(ishidden==true){
+            $('.header-menu').css({
+                display: 'flex',
+            });
+            ishidden=false;
+        }
+        else{
+            $('.header-menu').css({
+                display: 'none'
+            });
+            ishidden=true;
+        }
+        
+        // открываем-скрываем меню:
+        //$('.header-menu').toggle(500);
+        //$('.header-menu').animate({width: 'toggle'}, 500);
     });
 });
