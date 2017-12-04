b4a31d5d7689a22263128567fa2597b92c557c56
yegresvonason <yegresvonason@gmail.com>
2017-12-04 23:55:21 +0400

    media start

--- scripts.js
+++ scripts.js
@@ -1,10 +1,25 @@
-function showMenu(){
-    var menuContent = document.getElementById("menu-content");
+// function showMenu(){
+//     var menuContent = document.getElementById("menu-content");
 
-    if(menuContent.style.display != "block"){
-        menuContent.style.display = "block";
-    }
-    else{
-        menuContent.style.display = "none";
-    }
-}
+//     if(menuContent.style.display != "block"){
+//         menuContent.style.display = "block";
+//     }
+//     else{
+//         menuContent.style.display = "none";
+//     }
+// }
+
+// var menuContent = $("menu-content");
+// $("#button-header").on("click", function(event){
+// 	menuContent.show();
+// });
+
+$(document).ready(function(){
+	// $("#button-header").click(function(){
+	// 	$("#menu-content").show(1000);
+	// });
+
+	$("#button-header").click(function(){
+		$("#menu-content").toggle(800);
+	});
+});
