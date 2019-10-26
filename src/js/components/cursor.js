ed431bcbd8295149f2a128704a71684a6f97e771
Sergey Nosanov <yegresvonason@Macintosh.local>
2019-10-27 00:03:59 +0400

    add custom cursor --dev

--- src/js/components/cursor.js
+++ src/js/components/cursor.js
@@ -1,5 +1,22 @@
 function initCursor() {
-    console.log('cursor.js');
+    
+    const cursor = document.getElementById('cursor'),
+        cursorInner = document.getElementById('cursor-inner');
+
+    document.addEventListener('mousemove', function(event) {
+        console.log(event);
+
+        const x = event.clientX,
+            y = event.clientY;
+
+        cursorInner.style.transform = 'matrix(1, 0, 0, 1, ' + x + ', ' + y + '';
+        
+        setTimeout(() => {
+            cursor.style.transform = 'matrix(1, 0, 0, 1, ' + x + ', ' + y + '';
+        }, 50);
+
+    });
+
 }
 
 export default initCursor;
